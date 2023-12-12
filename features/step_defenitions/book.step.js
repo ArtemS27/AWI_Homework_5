511b8652f1e8062dc36203b4fcd2bced8cc79748
Artem <s-art7@bk.ru>
2023-12-12 19:12:34 +0200

    Make task 2

--- features/step_defenitions/book.step.js
+++ features/step_defenitions/book.step.js
@@ -1,40 +1,60 @@
-const {Given, When, Then, Before, After} = require('cucumber');
-const puppeteer = require('puppeteer');
-const expect = require('chai');
-const { clickElement, chooseSeat, bookSeats } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After } = require("cucumber");
+const puppeteer = require("puppeteer");
+const expect = require("chai");
+const {
+  clickElement,
+  chooseSeat,
+  bookSeats,
+} = require("../../lib/commands.js");
 
-Before(async function() {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 50});
-    const page = await browser.newPage();
-    this.browser = browser;
-    this.page = page;
+Before(async function () {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  this.browser = browser;
+  this.page = page;
 });
 
-After(async function() {
-    if(this.browser) {
-        await this.browser.close();
-    }
+After(async function () {
+  if (this.browser) {
+    await this.browser.close();
+  }
 });
 
-Given("user is on {string} page",{timeout: 30000}, async function (string) {
-    return await this.page.goto(string, {setTimeout: 10000});
+Given("user is on {string} page", { timeout: 60000 }, async function (string) {
+  return await this.page.goto(string);
 });
 
-When("user click on {string} day {string} seance and choose {int} row and {int} seat",{timeout: 30000}, async function (day, seance, row, seat) {
+When(
+  "user click on {string} day {string} seance and choose {int} row and {int} seat",
+  { timeout: 60000 },
+  async function (day, seance, row, seat) {
     await clickElement(this.page, day);
     await clickElement(this.page, seance);
     return await chooseSeat(this.page, row, seat);
-});
+  }
+);
 
-When("user click on {string} day {string} seance", {timeout: 30000}, async function (day, seance) {
+When(
+  "user click on {string} day {string} seance",
+  { timeout: 60000 },
+  async function (day, seance) {
     await clickElement(this.page, day);
     return await clickElement(this.page, seance);
-});
+  }
+);
 
-Then("user can book seat with {string} confirm button",{timeout: 30000}, async function (string) {
+Then(
+  "user can book seat with {string} confirm button",
+  { timeout: 60000 },
+  async function (string) {
     return await bookSeats(this.page, string);
-});
+  }
+);
 
-Then("user choose {int} row and {int} seat", {timeout: 30000}, async function (row, seat) {
-    await chooseSeat(this.page, 13, 24);
-});
+Then(
+  "user choose {int} row and {int} seat",
+  { timeout: 60000 },
+  async function (row, seat) {
+    expect(await chooseSeat(this.page, 13, 24)).to.throw('Invalid seat or row');
+  }
+);
